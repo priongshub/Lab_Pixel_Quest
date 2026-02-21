8a4755de82288ec405107892f643b3a67e70a5ee
priongshub <proingshu2011@gmail.com>
2026-02-21 16:21:38 -0500

    Update GeoController.cs

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeoController : MonoBehaviour
-{
+public class GeoController : MonoBehaviour { 
+    public int counter = 0;
     // Start is called before the first frame update
     void Start()
-    { float playerSpeed = 5.5f
+    {
+        float playerSpeed = 5.5f;
         Debug.Log("Hello World");
 
     }
@@ -14,7 +15,9 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
     }
-
 }                                                                         
