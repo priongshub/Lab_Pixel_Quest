e3d323cd6c5503fb03c1e76355535914f83d253d
priongshub <proingshu2011@gmail.com>
2026-03-22 17:34:31 -0400

    homework

--- Assets/Hands-On Homework #2/Scripts/HW2RightClickBullet.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2RightClickBullet.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
-public class HW2PlayerShoot : MonoBehaviour
+public class HW2PlayerShootRight : MonoBehaviour
 {
     public GameObject preFab;
     public Transform bulletTrash;
     public Transform bulletSpawn;
 
     private const float Timer = 0.5f;
-    private float _currentTime = 0.5f; 
+    private float _currentTime = 0.5f;
     private bool _canShoot = true;
+
+    public float speed = 10f; // NEW
 
     private void Update()
     {
@@ -16,18 +18,21 @@ public class HW2PlayerShoot : MonoBehaviour
         {
             _currentTime -= Time.deltaTime;
 
-            if(_currentTime < 0 )
+            if (_currentTime < 0)
             {
                 _canShoot = true;
                 _currentTime = Timer;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
         {
             GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
 
             bullet.transform.SetParent(bulletTrash);
+
+            // MOVE BULLET FORWARD
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.up * speed;
 
             _canShoot = false;
         }
